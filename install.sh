9d3f593ae1efd7d8140eb613e693a52527e8856b
sdob <sdob@cs.ucc.ie>
2017-11-22 12:01:15 +0000

    Add some TLS config settings

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 # This script runs before the Postfix service starts;
 # we do all our configuration stuff in here.
 
-# Configure Postfix-Postgres using the environment variables
-# we passed in
+################################
+# Configure Postfix-Postgresql #
+################################
+
 cat > /etc/postfix/pgsql-aliases.cf <<EOF
 hosts = $db_host
 user = $db_user
@@ -10,12 +12,18 @@ password = $db_password
 dbname = $db_name
 EOF
 
-# Set Postfix config options
+#####################
+# Configure Postfix #
+#####################
+
+# See http://flurdy.com/docs/postfix/
 
 # Set mail host
 postconf -e "myhostname=$mailhost"
+
 # Disable VRFY command
 postconf -e "disable_vrfy_command=yes"
+
 # Limit DOS attacks, courtesy of https://security-24-7.com/hardening-guide-for-postfix-2-x/
 postconf -e default_process_limit=100
 postconf -e smtpd_client_connection_count_limit=10
@@ -25,4 +33,15 @@ postconf -e header_size_limit=51200
 postconf -e message_size_limit=10485760
 postconf -e smtpd_recipient_limit=100
 
+# Configure TLS
+# TODO: Certs
+postconf -e smtp_tls_security_level=may
+postconf -e smtpd_tls_security_level=may
+postconf -e smtp_tls_note_starttls_offer=yes
+postconf -e smtpd_tls_loglevel=1
+postconf -e smtpd_tls_received_header=yes
+postconf -e smtpd_tls_session_cache_timeout=3600s
+postconf -e tls_random_source=dev:/dev/urandom
+
+# Check the config
 postfix check
