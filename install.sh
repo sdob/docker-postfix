4717aee8009d8b49604569049b884861e366f95c
sdob <sdob@cs.ucc.ie>
2017-11-22 11:50:33 +0000

    Add some hardening config rules

--- install.sh
+++ install.sh
@@ -14,8 +14,15 @@ EOF
 
 # Set mail host
 postconf -e "myhostname=$mailhost"
-
 # Disable VRFY command
 postconf -e "disable_vrfy_command=yes"
+# Limit DOS attacks, courtesy of https://security-24-7.com/hardening-guide-for-postfix-2-x/
+postconf -e default_process_limit=100
+postconf -e smtpd_client_connection_count_limit=10
+postconf -e smtpd_client_connection_rate_limit=30
+postconf -e queue_minfree=20971520
+postconf -e header_size_limit=51200
+postconf -e message_size_limit=10485760
+postconf -e smtpd_recipient_limit=100
 
 postfix check
